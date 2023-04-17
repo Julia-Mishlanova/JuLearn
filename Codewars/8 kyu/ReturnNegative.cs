52160fd8f4c38ec6dd2a5f2e776eae580231106b
Julia Mishlanova <112238047+Julia-Mishlanova@users.noreply.github.com>
2023-04-17 21:14:35 +0300

    Add files via upload

---
+++ Codewars/8 kyu/ReturnNegative.cs
@@ -0,0 +1,15 @@
+using System;
+
+public static class Kata
+{
+  public static int MakeNegative(int number)
+  {
+    // Code?
+    if(number > 0)
+    {
+      return - number;
+    }
+    
+    return number;
+  }
+}
