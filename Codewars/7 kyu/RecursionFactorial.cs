72afd60e92aad14330c1913e512fe894c16388bc
Julia Mishlanova <112238047+Julia-Mishlanova@users.noreply.github.com>
2023-04-28 20:24:24 +0300

    Add files via upload

---
+++ Codewars/7 kyu/RecursionFactorial.cs
@@ -0,0 +1,18 @@
+public class Recursion
+{
+  public static ulong Factorial(ulong n)
+  {
+    checked
+    {
+      if(n == 0)
+      {
+        return 1;
+      }
+      if(n == 1)
+      {
+        return 1;
+      }
+      return n * Factorial(n-1);
+    }
+  }
+}
