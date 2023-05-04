1d49eabeb54ccb007d127c262b5d64d734f02c2f
Julia Mishlanova <112238047+Julia-Mishlanova@users.noreply.github.com>
2023-05-04 20:50:13 +0300

    Add files via upload

---
+++ Codewars/8 kyu/ReturningString.cs
@@ -0,0 +1,10 @@
+using System;
+
+public static class Kata
+{
+  public static string Greet(string name)
+  {
+            string gr = "Hello, " + name + " how are you doing today?";
+            return gr;
+  }
+}
