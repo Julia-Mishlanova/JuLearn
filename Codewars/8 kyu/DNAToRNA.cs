a79e8e9108c4fbeef35b8f2d0f5c8e2c58087789
Julia Mishlanova <112238047+Julia-Mishlanova@users.noreply.github.com>
2023-05-04 20:47:49 +0300

    Add files via upload

---
+++ Codewars/8 kyu/DNAToRNA.cs
@@ -0,0 +1,12 @@
+namespace Converter 
+{
+  public class Converter
+  {
+    public string dnaToRna(string dna)
+    {
+      // Have fun! ^_^
+            string go = dna.Replace( "T" , "U");
+            return go;
+    }
+  }
+}
