426a0883a86d11137b4eb905e282bda0c0d9fc79
Julia Mishlanova <112238047+Julia-Mishlanova@users.noreply.github.com>
2023-05-25 14:40:29 +0300

    Add files via upload

---
+++ Codewars/8 kyu/ReturnTheDay.cs
@@ -0,0 +1,24 @@
+public class Kata
+{
+  public static string WhatDay(int n)
+  {
+            if (n < 1 || n > 7) return "Wrong, please enter a number between 1 and 7";
+    
+            string[] dayOfWeek = 
+            { 
+                "Sunday", 
+                "Monday", 
+                "Tuesday", 
+                "Wednesday", 
+                "Thursday", 
+                "Friday", 
+                "Saturday" 
+            };
+
+            for (int i = 0; i < dayOfWeek.Length; i++)
+            {
+                if (i == n - 1) return dayOfWeek[i];
+            }
+            return string.Empty;
+  }
+}
